3398236f138a46bb325299a5d5b2bfa83f4f1550
Alexa <alexa4berg@gmail.com>
2018-12-11 00:32:49 -0500

    Adding JS File

---
+++ project3Portfolio/script.js
@@ -0,0 +1,9 @@
+ // HAMBURGER MENU EVENT STUFF //
+ // http://michalsnik.github.io/aos/
+ // https://stackoverflow.com/questions/45425275/stop-scrolling-effect-happening-when-going-up
+
+ function checkedBox(cb) {
+     console.log("Click");
+     $("#menu").slideToggle("slow");
+ }
+

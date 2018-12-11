8009aacb2b12e9379914630c27e83f74d8a8fe19
Alexa <alexa4berg@gmail.com>
2018-12-11 00:34:18 -0500

    use strict

--- project3Portfolio/script.js
+++ project3Portfolio/script.js
@@ -1,6 +1,7 @@
  // HAMBURGER MENU EVENT STUFF //
  // http://michalsnik.github.io/aos/
  // https://stackoverflow.com/questions/45425275/stop-scrolling-effect-happening-when-going-up
+"use strict";
 
  function checkedBox(cb) {
      console.log("Click");

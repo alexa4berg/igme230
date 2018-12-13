c892c8783c1a2ee1dbfa164f84a9613065e7dc53
Alexa <alexa4berg@gmail.com>
2018-12-13 13:06:37 -0500

    Fixed html

--- fall2018makeup/fall2018finalmakeup.js
+++ fall2018makeup/fall2018finalmakeup.js
@@ -10,6 +10,11 @@ let selection = ("story0.txt")
 $("#story").val(selection);
 $("#story").load(selection);
 
+// changing the story
+$("#menu").change(function() { //on change, when a new menu item is selected do this
+verse = $(this).val(); //the verse which is my variable for the content information is set to the new value of the selected menu item.
+$("#content").load(verse); //upload the new verse and replace the old one
+});
 
 // extra credit clicks
 let clicks = 0;
@@ -17,4 +22,13 @@ let clicks = 0;
                 clicks++;
                 $("#currentcount").html(clicks);
             })
+
+$("#submenu").change(function() { //on change, when a new menu item is selected do this
+color = $(this).val(); //the verse which is my variable for the content information is set to the new value of the selected menu item.
+$("#source").css("background-color", "color"); //upload the new verse and replace the old one
+});
+
+
+
+
 

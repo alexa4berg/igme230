9c2f59224afc5c598642b8f300d1311472a411e8
Alexa <alexa4berg@gmail.com>
2018-12-13 13:10:25 -0500

    Adding selecting color

--- fall2018makeup/fall2018finalmakeup.js
+++ fall2018makeup/fall2018finalmakeup.js
@@ -11,9 +11,9 @@ $("#story").val(selection);
 $("#story").load(selection);
 
 // changing the story
-$("#menu").change(function() { //on change, when a new menu item is selected do this
-verse = $(this).val(); //the verse which is my variable for the content information is set to the new value of the selected menu item.
-$("#content").load(verse); //upload the new verse and replace the old one
+$("#colors").change(function() { //on change, when a new menu item is selected do this
+color = $(this).val(); //the verse which is my variable for the content information is set to the new value of the selected menu item.
+$("#source").css("background-color", "color"); //upload the new verse and replace the old one
 });
 
 // extra credit clicks
@@ -22,11 +22,6 @@ let clicks = 0;
                 clicks++;
                 $("#currentcount").html(clicks);
             })
-
-$("#submenu").change(function() { //on change, when a new menu item is selected do this
-color = $(this).val(); //the verse which is my variable for the content information is set to the new value of the selected menu item.
-$("#source").css("background-color", "color"); //upload the new verse and replace the old one
-});
 
 
 

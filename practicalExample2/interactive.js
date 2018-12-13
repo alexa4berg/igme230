85d9ffcf2936ce4073b64ea4df1cb89cc13f1cd8
Alexa <alexa4berg@gmail.com>
2018-12-13 09:29:35 -0500

    practicing second example

--- practicalExample2/interactive.js
+++ practicalExample2/interactive.js
@@ -1,16 +1,17 @@
-$(".menu").click(function () {
-    $(this).next(".items").slideToggle();
+$(".menu").click(function () { // when menu is clicked
+    $(this).next(".items").slideToggle(); //the selected menus next items slide toggle
 });
 
 
 
+// sets the initial article txt
 
 let selection = ("article0.txt")
 
 $("article").val(selection);
 $("article").load(selection);
 
-
+// when a radio button is selected load that values txt
 $('input[type=radio][name=article]').change(function () {
             selection = $(this).val();
             $("article").load(selection);
@@ -18,7 +19,7 @@ $('input[type=radio][name=article]').change(function () {
 });
 
 
-
+// clicks
 let clicks = 0;
             $("#clickme").click(function () {
                 clicks++;

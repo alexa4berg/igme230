5d9beafbdae89aa3215a600b1dda1255912f8c09
Alexa <alexa4berg@gmail.com>
2018-12-14 12:12:17 -0500

    Fixes after meeting

--- fall2018makeup/fall2018finalmakeup.js
+++ fall2018makeup/fall2018finalmakeup.js
@@ -1,4 +1,4 @@
-$(".menu").click(function(){ //on click of my menu option
+$(".menu").click(function () { //on click of my menu option
     $(this).next(".submenu").slideToggle(); //reveal the sub menu items
 });
 
@@ -11,19 +11,14 @@ $("#story").val(selection);
 $("#story").load(selection);
 
 // changing the color
-$("#colors").change(function() { //on change, when a new menu item is selected do this
-color = $(this).attr('id'); //the verse which is my variable for the content information is set to the new value of the selected menu item.
-$("#source").css("background-color", "color"); //upload the new verse and replace the old one
+$("#colors").change(function () { //on change, when a new menu item is selected do this
+    color = $(this).attr('id'); //the verse which is my variable for the content information is set to the new value of the selected menu item.
+    $("#source").css("background-color", "color"); //upload the new verse and replace the old one
 });
 
 // extra credit clicks
 let clicks = 0;
-            $("#increment").click(function () {
-                clicks++;
-                $("#currentcount").html(clicks);
-            })
-
-
-
-
-
+$("#increment").click(function () {
+    clicks++;
+    $("#currentcount").html(clicks);
+})
